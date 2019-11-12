d556be05fa4b7a56a7042c59a010631de890c8c0
FidelDhlamini <goodkidfidel@gmail.com>
2019-11-12 12:47:11 +0200

    added unit tests for my database code

--- test/database.test.js
+++ test/database.test.js
@@ -17,27 +17,79 @@ const pool = new Pool({
 });
 
 describe('Greetings-web-app', function () {
-    const greetings = Greetings()
+    const greetings = Greetings(pool)
 
-    beforeEach(async function () {
-        // clean the tables before each test run
-        await pool.query("delete from products;");
-        await pool.query("delete from categories;");
-    });
+    // beforeEach(async function () {
+    //     // clean the tables before each test run
+    //     await pool.query("delete from products;");
+    //     await pool.query("delete from categories;");
+    // });
 
-    it('should pass the db test', async function () {
-        
+    it('should greet the name entered in Xhosa', async function () {
 
-        // the Factory Function is called CategoryService
-        let categoryService = CategoryService(pool);
-        await categoryService.add({
-            description: "Diary"
-        });
-
-        let categories = await categoryService.all();
-        assert.equal(1, categories.length);
+        await greetings.greet("Fidel", "Xhosa")
+        assert.equal("Molo, Fidel", await greetings.greetMsg());
 
     });
+
+    it('should greet the name entered in English', async function () {
+
+        await greetings.greet("Sino", "English")
+        assert.equal("Hello, Sino", await greetings.greetMsg());
+
+    });
+
+    it('should greet the name entered in Afrikaans', async function () {
+
+        await greetings.greet("Iviwe", "Afrikaans")
+        assert.equal("Hallo, Iviwe", await greetings.greetMsg());
+
+    });
+
+    it('should how many names were greeted ', async function () {
+        await greetings.greet("Iviwe", "Afrikaans")
+        await greetings.greet("Fidel", "Afrikaans")
+        await greetings.greet("Sbu", "Afrikaans")
+        await greetings.greet("Sino", "Afrikaans")
+
+        assert.equal(4, await greetings.numberOfGreetedNames());
+
+    });
+
+    it('should count a name once if it is greeted repeatedly ', async function () {
+        await pool.query('delete from greetings;')
+        await greetings.greet("Iviwe")
+        await greetings.greet("Iviwe")
+        await greetings.greet("Iviwe")
+        await greetings.greet("Iviwe")
+
+        assert.equal(1, await greetings.numberOfGreetedNames());
+
+    });
+    it('should return how many times a specific name was greeted', async function () {
+        await pool.query('delete from greetings;')
+        await greetings.greet("Sino")
+        await greetings.greet("Sino")
+        await greetings.greet("Sino")
+        await greetings.greet("Iviwe")
+
+
+        assert.equal(3, await greetings.perName("Sino"));
+        assert.equal(1, await greetings.perName("Iviwe"));
+
+    });
+it('should return how many times a specific name was greeted', async function () {
+    await pool.query('delete from greetings;')
+    await greetings.greet("Sino")
+    await greetings.greet("Sino")
+    await greetings.greet("Sino")
+    await greetings.greet("Iviwe")
+
+
+    assert.equal(3, await greetings.perName("Sino"));
+    assert.equal(1, await greetings.perName("Iviwe"));
+
+});
 
     after(function () {
         pool.end();
